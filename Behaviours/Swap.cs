6c9be9665b87fcfa944fe9d551f4ff862d06ac4d
Helgi Helgason <HelgiHel@gmail.com>
2016-11-26 15:58:16 +0000

    created tutorial

--- Behaviours/Swap.cs
+++ Behaviours/Swap.cs
@@ -4,164 +4,46 @@ using System.Collections.Generic;
 
 public class Swap : MonoBehaviour {
 
-	public delegate void Swapped ();
-	public static event Swapped OnSwap;
-
-
 	[SerializeField]
 	Material matGlass;
 	[SerializeField]
 	Material matTex;
+	[SerializeField]
+	GameObject platform;
 
-	//Parent objects of dark and light objects
-	public GameObject objectsParent;
-
-	//List of the objects
-	public List<GameObject> Objects;
-
-
-	//true on dark platforms
-	public bool isLightDimension = true;
-
-	//Ref to the swapeffect
-	SwapEffect swapEffect;
-
-	void Awake () {
-
-		GetObjects ();
-	}
+	Renderer rend;
+	Collider col;
 
 	// Use this for initialization
 	void Start () {
-
-		
-		swapMaterial ();
-
-		swapEffect = GetComponent<SwapEffect> ();
+		rend = platform.GetComponent<Renderer> ();
+		col = platform.GetComponent<Collider> ();
 	}
 
 	void OnEnable(){
 		UserInputHandler.OnLeftTap += swapMaterial;
-		UserInputHandler.OnLeftTap += SwapDimensions;
-		Death.playerDied += ResetMaterials;
 	}
 
 	void OnDisable () {
 		UserInputHandler.OnLeftTap -= swapMaterial;
-		UserInputHandler.OnLeftTap -= SwapDimensions;
-		Death.playerDied -= ResetMaterials;
-
-
 	}
 
 	
 	// Update is called once per frame
 	void Update () {
 		
-		if(Input.GetKeyDown(KeyCode.L)) {
-			swapMaterial ();
-			isLightDimension = !isLightDimension;
-		}
+	
 	}
-
-
-
-	void GetObjects() {
-
-		Transform [] DarkObj = objectsParent.GetComponentsInChildren<Transform>();
-
-		Objects = new List<GameObject>(); //listinn sem er notaður
-
-		foreach (Transform darkObject in DarkObj) {
-
-			//bætir við child hlutunum í dark Listann
-			if (darkObject != null && darkObject != DarkObj[0]) {
-				Objects.Add(darkObject.gameObject);
-
-			}
-		}
-	}
-
+		
 	void swapMaterial () {
-
-
-		//Calls on the onSwap event and triggers the glitchEffect
-		if (OnSwap != null) {
-			OnSwap ();
-		}
-
-
-
-		//function swaps the material on All platform excep the first one
-		foreach (GameObject darkObj in Objects) {
-
-			//creating random dimension
-			int rand = (int)Random.Range (0, 2);
-
-			switch (rand) {
-			case 0:
-				isLightDimension = true;
-				break;
-			case 1:
-				isLightDimension = false;
-				break;
-			}
-
-			//Getting the object ready
-			Renderer materialRenderer;
-			materialRenderer = darkObj.GetComponent<Renderer>();
-
-			//swaps the status of the object
-			Status status = darkObj.GetComponent<Status> ();
-			status.isSelected = !status.isSelected;
-
-			//Getting the Collider
-			Collider col;
-			col = darkObj.GetComponent<Collider> ();
-
-			if (!isLightDimension) {
-				materialRenderer.material = matGlass;
-			//	col.enabled = false;
-				col.isTrigger = true;
-
-
-			} else {
-				materialRenderer.material = matTex;
-
-			//	col.enabled = true;
-				col.isTrigger = false;
-			}
-		}
-	}
-
-	void disableColliders(List<GameObject> objs){
-
-		foreach(GameObject someObject in objs) {
-
-			Collider col = someObject.GetComponent<Collider> ();
+		
+		if (rend.material.name == "matTex (Instance)") {
+			rend.material = matGlass;
 			col.isTrigger = true;
+		} else if(rend.material.name == "glassy (Instance)"){
+			rend.material = matTex;
+			col.isTrigger = false;
 		}
-	}
 
-	void SwapDimensions () {
-		isLightDimension = !isLightDimension;
-	}
-		
-	void ResetMaterials() {
-
-		/*if (!isLightDimension) {
-			swapMaterial ();
-			isLightDimension = true;
-		}*/
-		StartCoroutine (waitForReset (0.4f));
-	}
-
-	IEnumerator waitForReset (float waitTime){
-		
-		yield return new WaitForSeconds(waitTime);
-		if (!isLightDimension) {
-			swapMaterial ();
-			isLightDimension = true;
-		}
 	}
 }
---
+++ Managers/Tutorial.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+
+	/*public GameObject gameManagerObj;
+	MoreMountains.InfiniteRunnerEngine.GameManager gameManger;
+*/
+	public Text instrucitonTxt;
+	public float timer;
+
+	public float firstPart;
+	public float secondPart;
+	public float thirdPart;
+	public float fourthPart;
+	public float fifithPart;
+	public float ending;
+
+	// Use this for initialization
+	void Awake () {
+		//gameManger = gameManagerObj.GetComponent<MoreMountains.InfiniteRunnerEngine.GameManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		timer += Time.deltaTime;
+
+		if (timer > firstPart && timer < secondPart) {
+			instrucitonTxt.text = "Tap the right side of the screen to jump";
+		} else if (timer > secondPart && timer < thirdPart) {
+			instrucitonTxt.text = "Hold to jump longer";
+		} else if (timer > thirdPart && timer < fourthPart) {
+			instrucitonTxt.text = "You can also double jump";
+		} else if (timer > fourthPart && timer < fifithPart) {
+			instrucitonTxt.text = "When a platform is semi-transparent, you need to \"Swap\" by tapping the left side of the screen";
+			StartCoroutine (SlowTime (3f));
+		} else if (timer < firstPart) {
+			instrucitonTxt.text = "";
+		}
+		else {
+			instrucitonTxt.text = "Good job, have fun";
+		}
+	}
+
+	IEnumerator SlowTime(float waitTime) {
+		
+		float currentTimeScale = Time.timeScale;
+		Time.timeScale = 0.5f;
+		yield return new WaitForSeconds(waitTime);
+		Time.timeScale = currentTimeScale;
+	}
+}
