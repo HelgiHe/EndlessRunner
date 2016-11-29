17e38c7c47b461fb690f6270ae34c34cb44ef496
Helgi Helgason <HelgiHel@gmail.com>
2016-11-29 19:18:52 +0000

    highscore Fix

---
+++ Utilities/deleteSave.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deleteSave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		if(ES2.Exists("highScore"))
+			ES2.Delete("highScore");
+	}
+	
+
+}
